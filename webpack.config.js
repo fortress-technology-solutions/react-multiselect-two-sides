dd1f4ea30ec28c5533f1ee8429679e1a9d9f55b8
Frank <frankhn0801@gmail.com>
2018-02-02 12:13:30 -0600

    added externals object

--- webpack.config.js
+++ webpack.config.js
@@ -38,9 +38,18 @@ module.exports = {
 		]
 	},
 	externals: {
-		react: 'React',
-		'react-dom': 'ReactDOM',
-		'prop-types': 'PropTypes'
+		react: {
+			root: 'React',
+			commonjs2: 'react',
+			commonjs: 'react',
+			amd: 'react'
+		},
+		'react-dom': {
+			root: 'ReactDOM',
+			commonjs2: 'react-dom',
+			commonjs: 'react-dom',
+			amd: 'react-dom'
+		}
 	},
 	devtool: 'eval',
 	resolve: {
